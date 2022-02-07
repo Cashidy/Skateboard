26c48754c32b4c12a177b99852606629c4f0d435
$uper Cash <scashcassidy@gmail.com>
2022-02-07 09:55:29 -0800

    Update Skateboard.js

--- Skateboard.js
+++ Skateboard.js
@@ -10,44 +10,79 @@ function main() {
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 5;
+    camera.position.z = 1;
   
     const scene = new THREE.Scene();
-    
+
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
     scene.add(light);
     
-    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
     material.side = THREE.DoubleSide;
 
-    var flatVs00 = [0.0000, 0.0000, 0.0000];
-    var flatVs01 = [0.0000, 0.0100, 0.0800];
-    var flatVs02 = [0.0000, 0.0159, 0.1048];
-    var flatVs10 = [0.1650, 0.0000, 0.0000];
-    var flatVs11 = [0.1650, 0.0100, 0.0800];
-    var flatVs12 = [0.1650, 0.0159, 0.1408];
-    var flatVs20 = [0.1969, 0.0127, 0.0000];
-    var flatVs21 = [0.1969, 0.0127, 0.0800];
-    var flatVs22 = [0.1969, 0.0159, 0.1048];
+    const boardLength = 0.81280162560;
+    const boardWidth = 0.20955041910;
+    const lipLength = boardWidth/2;
+    const crease = boardLength/2 - lipLength;
+    const drop = crease - 0.04;
+    const edgeHeight = 0.01587503175;
+    const edgeDepth = (boardWidth/2) * 5/6;
+    const lipRise = 0.05397510795;
 
-    var flatTris = [
-    [flatVs00, flatVs10, flatVs01],
-    [flatVs10, flatVs11, flatVs01],
-    [flatVs10, flatVs20, flatVs11],
-    [flatVs20, flatVs21, flatVs11],
-    [flatVs01, flatVs11, flatVs02],
-    [flatVs11, flatVs12, flatVs02],
-    [flatVs11, flatVs21, flatVs12],
-    [flatVs21, flatVs22, flatVs12]
+    var quartVerts = [
+    [[0, edgeHeight, boardWidth/2],[drop, edgeHeight, boardWidth/2],[crease, edgeHeight, boardWidth/2],
+        [Math.cos(5*Math.PI/12)*lipLength + crease, Math.cos(5*Math.PI/12)*lipRise, Math.sin(5*Math.PI/12)*lipLength],
+        [Math.cos(Math.PI/3)*lipLength + crease, Math.cos(Math.PI/3)*lipRise, Math.sin(Math.PI/3)*lipLength],
+        [Math.cos(Math.PI/4)*lipLength + crease, Math.cos(Math.PI/4)*lipRise, Math.sin(Math.PI/4)*lipLength],
+        [Math.cos(Math.PI/6)*lipLength + crease, Math.cos(Math.PI/6)*lipRise, Math.sin(Math.PI/6)*lipLength],
+        [Math.cos(Math.PI/12)*lipLength + crease, Math.cos(Math.PI/12)*lipRise, Math.sin(Math.PI/12)*lipLength],
+        [boardLength/2, (boardLength/2 - crease) * lipRise, 0]],
+    [[0, edgeHeight * 2/3, edgeDepth],[drop, edgeHeight * 2/3, edgeDepth],[crease, edgeHeight * 5/6, edgeDepth],[(Math.cos(5*Math.PI/12)*lipLength)*3/4 + crease, (Math.cos(5*Math.PI/12)*3/4)*lipRise, (Math.sin(5*Math.PI/12)*lipLength)*3/4]],
+    [[0, 0, 0],[drop, 0, 0],[crease, 0, 0]]]; 
+
+    var tris = [
+        [quartVerts[2][0], quartVerts[2][1], quartVerts[1][0]],
+        [quartVerts[2][1], quartVerts[1][1], quartVerts[1][0]],
+        [quartVerts[1][0], quartVerts[1][1], quartVerts[0][0]],
+        [quartVerts[1][1], quartVerts[0][1], quartVerts[0][0]],
+        [quartVerts[2][1], quartVerts[2][2], quartVerts[1][1]],
+        [quartVerts[2][2], quartVerts[1][2], quartVerts[1][1]],
+        [quartVerts[1][1], quartVerts[1][2], quartVerts[0][1]],
+        [quartVerts[1][2], quartVerts[0][2], quartVerts[0][1]],
+        [quartVerts[1][2], quartVerts[1][3], quartVerts[0][2]],
+        [quartVerts[1][3], quartVerts[0][3], quartVerts[0][2]],
+        [quartVerts[2][2], quartVerts[1][3], quartVerts[1][2]],
+        [quartVerts[2][2], quartVerts[0][4], quartVerts[0][3]],
+        [quartVerts[2][2], quartVerts[0][5], quartVerts[0][4]],
+        [quartVerts[2][2], quartVerts[0][6], quartVerts[0][5]],
+        [quartVerts[2][2], quartVerts[0][7], quartVerts[0][6]],
+        [quartVerts[2][2], quartVerts[0][8], quartVerts[0][7]]
     ]
+
+
     const positions = [];
-    for (const tri of flatTris) {
-        positions.push(...tri[0]);
-        positions.push(...tri[1]);
-        positions.push(...tri[2]);
+    for (const tri of tris) {
+        positions.push(tri[0]);
+        positions.push(tri[1]);
+        positions.push(tri[2]);
+    }
+    for (const tri of tris) {
+        positions.push(-tri[0]);
+        positions.push(tri[1]);
+        positions.push(tri[2]);
+    }
+    for (const tri of tris) {
+        positions.push(-tri[0]);
+        positions.push(tri[1]);
+        positions.push(-tri[2]);
+    }
+    for (const tri of tris) {
+        positions.push(tri[0]);
+        positions.push(tri[1]);
+        positions.push(-tri[2]);
     }
 
     const board = new THREE.BufferGeometry();
